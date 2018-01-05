dd2c3795da91a2b0ff2d88c6ccd6e2f85eb75d02
ulfsjodin <sjodin.ulf@gmail.com>
2018-01-05 21:47:47 +0100

    more 'playable'

--- rock_bag.py
+++ rock_bag.py
@@ -1,28 +1,32 @@
 from random import randint
 
 computer_choices = {1:'rock', 2:'paper', 3:'scissor'}
-players_choice = raw_input('your choice >' )
 win_loose = [('scissor','paper'), ('rock','scissor'), ('paper','rock')] 
 rand = computer_choices.get(randint(1,3))
 
 def test_win_opt():
-    game = (players_choice, rand)
     players_points = 0
     computers_points = 0
-    print '-' * 50
-    print 'computer choices' ,rand 
-    print '-' * 50
-    if players_choice ==rand:
-        print('even')
-    elif game in win_loose:
-        print('Player wins')
-        players_points +=1
-    else:
-        print('computer wins')
-        computers_points += 1
-    print '*' * 50
+    while True:
+        players_choice = raw_input('your choice >' )
+        game = (players_choice, rand)
+        print '-' * 50
+        print 'computer choices' ,rand 
+        print '-' * 50
+        if players_choice ==rand:
+            print('even')
+        elif game in win_loose:
+            print('Player wins')
+            players_points +=1
+        else:
+#            return False
 
-    print 'player:', players_points, 'computer:', computers_points
+            print('computer wins')
+            computers_points += 1
+            print '*' * 50
     
+        print 'player:', players_points, 'computer:', computers_points
+        if players_points >4:
+            return False
 
 test_win_opt()
