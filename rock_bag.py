59204ad86d4773d459612a2bfdafb4a8d3c0a37f
ulfsjodin <sjodin.ulf@gmail.com>
2018-01-08 18:59:05 +0100

    corrected randint() and added computer wins at 5 points

--- rock_bag.py
+++ rock_bag.py
@@ -4,13 +4,12 @@ import random
 computer_choices = {1:'rock', 2:'paper', 3:'scissor'}
 win_loose = [('scissor','paper'), ('rock','scissor'), ('paper','rock')] 
 value = random.randint(1,3)
-rand = computer_choices.get(value)
 
 def scissor_game():
-    #rand = computer_choices.get(random.randint(1,3))
     players_points = 0
     computers_points = 0
     while True:
+        rand = computer_choices.get(random.randint(1,3))
         players_choice = raw_input('your choice >' )
         game = (players_choice, rand)
         print '-' * 50
@@ -18,18 +17,21 @@ def scissor_game():
         print '-' * 50
         if players_choice ==rand:
             print('even')
+            print '*' * 50
         elif game in win_loose:
             print('Player wins')
-            print '/' * 50
+            print '+' * 50
             players_points +=1
         else:
             print('computer wins')
             computers_points += 1
-            print '*' * 50
+            print '-' * 50
     
         print 'player:', players_points, 'computer:', computers_points
-        print '-' * 50
+        print '%' * 50
         if players_points >4:
+            return False
+        if computers_points >4:
             return False
 
 scissor_game()
